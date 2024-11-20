53bad8146a9653ad49274d8d5c0dd0c9b6bcbd76
Thiara1998 <thiarakante@gmal.com>
2024-11-20 11:54:07 +0000

    lampe commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Add your repo name here
+  // base: '/portfolio/', // Replace 'portfolio' with your subpath
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
